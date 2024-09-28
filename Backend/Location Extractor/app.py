1b0a7300f3cf97b0be63fd9fcd4aa73818635c96
Ilham <ilhamsyed1310@gmail.com>
2024-09-29 00:57:47 +0530

    Added marker for localizing the disaster

--- Backend/Location Extractor/app.py
+++ Backend/Location Extractor/app.py
@@ -1,23 +1,28 @@
+from flask import Flask, jsonify
 import PIL.Image
 import PIL.ExifTags
+from flask_cors import CORS  # Import the CORS package
 
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
-img= PIL.Image.open('DSCN0012.jpg')
-exif={
-    PIL.ExifTags.TAGS[k]: v
-    for k, v in img._getexif().items()
-    if k in PIL.ExifTags.TAGS
-}
+@app.route('/coordinates', methods=['GET'])
+def get_coordinates():
+    img = PIL.Image.open('DSCN0012.jpg')
+    exif = {
+        PIL.ExifTags.TAGS[k]: v
+        for k, v in img._getexif().items()
+        if k in PIL.ExifTags.TAGS
+    }
 
-print (exif.get('GPSInfo'))
+    gps_info = exif.get('GPSInfo')
+    north = gps_info.get(2)
+    east = gps_info.get(4)
 
-north=exif.get('GPSInfo').get(2)
-east=exif.get('GPSInfo').get(4)
+    lat = ((((north[0]*60)+north[1])*60)+north[2])/60/60
+    long = ((((east[0]*60)+east[1])*60)+east[2])/60/60
 
+    return jsonify({'latitude': float(lat), 'longitude': float(long)})
 
-lat=((((north[0]*60)+north[1])*60)+north[2])/60/60
-long=((((east[0]*60)+east[1])*60)+east[2])/60/60
-
-lat,long=float(lat),float(long)
-print(lat,"  ",long)
-
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
