cbc64da94a383c6f7604232bf9c08cbce350690d
Ilham-Syed <ilhamsyed1310@gmail.com>
2024-09-28 20:19:24 +0530

    Working on leaflet

--- Frontend/EmergeSense_Website/src/main.jsx
+++ Frontend/EmergeSense_Website/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Map from './Components/map.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Map />
   </StrictMode>,
 )
