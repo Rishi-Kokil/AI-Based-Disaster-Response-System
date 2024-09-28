97e25ea2a2b3bcd163235f73d30d3f86a348dff2
Rishi Kokil <rishikokil@gmail.com>
2024-09-28 23:52:57 +0530

    Added Laflet to the Frontend to fetch Map Data

--- Frontend/EmergeSense_Website/src/main.jsx
+++ Frontend/EmergeSense_Website/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Map from './Components/map.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Map />
+    <App />
   </StrictMode>,
 )
