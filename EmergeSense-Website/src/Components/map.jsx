268b350f3b6a9cf8c55e6ef8dc29a2c19eafc8a6
Rishi Kokil <rishikokil@gmail.com>
2024-10-01 20:07:26 +0530

    Added Tailwind CSS to our website

--- EmergeSense-Website/src/Components/map.jsx
+++ EmergeSense-Website/src/Components/map.jsx
@@ -4,19 +4,8 @@ import 'leaflet/dist/leaflet.css';
 
 const MapComponent = () => {
   const [mapData, setMapData] = useState(null);
-  const [coordinates, setCoordinates] = useState({ latitude: null, longitude: null });
   const [opacity, setOpacity] = useState(1);
 
-  useEffect(() => {
-    // Fetch the coordinates from the backend Flask app
-    fetch('http://localhost:5050/coordinates')
-      .then(response => response.json())  // Make sure to return the parsed JSON
-      .then(data => {
-        console.log('Fetched coordinates:', data); // Log the fetched data for debugging
-        setCoordinates(data);  // Set the coordinates in the state
-      })
-      .catch(error => console.error('Error fetching coordinates:', error));
-  }, []);
 
   useEffect(() => {
     const fetchMapData = async () => {
@@ -42,12 +31,12 @@ const MapComponent = () => {
   return (
     <div>
       {/* Check if coordinates are loaded */}
-      {coordinates.latitude && coordinates.longitude && mapData ? (
+      { mapData ? (
         <div>
           {/* Render the map using Leaflet */}
           <MapContainer
-            center={[coordinates.latitude, coordinates.longitude]} // Center the map at the fetched coordinates
-            zoom={13}
+            center={[11, 11]} // Center the map at the fetched coordinates
+            zoom={5}
             style={{ height: '400px', width: '100%' }}
           >
             <TileLayer
@@ -63,15 +52,8 @@ const MapComponent = () => {
               opacity={opacity} // Set the opacity of the tile layer
               attribution="&copy; <a href=&quot;https://earthengine.google.com/&quot;>Google Earth Engine</a> contributors"
             />
-            {/* Marker to show the location */}
-            <Marker position={[coordinates.latitude, coordinates.longitude]}>
-              <Popup>
-                Latitude: {coordinates.latitude}, Longitude: {coordinates.longitude}
-              </Popup>
-            </Marker>
           </MapContainer>
 
-          {/* Slider to control opacity of the map tiles */}
           <div style={{ margin: '10px' }}>
             <label htmlFor="opacitySlider">Opacity: {opacity}</label>
             <input
