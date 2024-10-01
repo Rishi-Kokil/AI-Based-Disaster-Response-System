ed9256f808b8b51c0984096aef26bfff3be07273
Rishi Kokil <rishikokil@gmail.com>
2024-10-01 20:07:26 +0530

    Added Tailwind CSS to our website

--- EmergeSense-Website/src/App.jsx
+++ EmergeSense-Website/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import './App.css'
 import MapComponent from './Components/map'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <h1> Map is Printed below</h1>
+        <h1>
+          Map Previewd Here
+        </h1>
         <MapComponent />
       </div>
-      
+
     </>
   )
 }
