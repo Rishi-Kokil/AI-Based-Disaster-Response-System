4ee7edce2adcdf30d02c82dd7e8af124332dd0e5
Rishi Kokil <rishikokil@gmail.com>
2025-01-30 19:11:50 +0530

    chore: revert website to Vite template, restarting development

--- EmergeSense-Website/src/App.jsx
+++ EmergeSense-Website/src/App.jsx
@@ -1,17 +1,15 @@
-import MapComponent from './Components/map'
+import React from 'react'
 
 function App() {
   return (
     <>
-      <div>
-        <h1>
-          Map Previewd Here
-        </h1>
-        <MapComponent />
-      </div>
-
+      <h1
+        className='text-4xl text-center absolute transform -translate-y-1/2 top-1/2 -translate-x-1/2 left-1/2'
+      >  
+          Admin Panel EmergeSense
+      </h1>
     </>
   )
 }
 
-export default App
+export default App;
