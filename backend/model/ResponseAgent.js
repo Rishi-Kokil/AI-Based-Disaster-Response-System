2fbf92d0c8f5eb355fa874f72581400d8e071d03
Ilham <ilhamsyed1310@gmail.com>
2025-03-20 10:34:08 +0530

    Splitting response agency router

--- backend/model/ResponseAgent.js
+++ backend/model/ResponseAgent.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
-
 const responseAgentSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password_hash: { type: String, required: true }, // Hashed password for security
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now },
+});
+
+const responseAgentProfileSchema = new mongoose.Schema({
+    agent_id: { type: mongoose.Schema.Types.ObjectId, ref: 'ResponseAgent', required: true }, // Foreign Key to ResponseAgent
     agency_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Agency', required: true }, // Foreign Key to Agency
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
     phone_number: { type: String, required: true },
-    password_hash: { type: String, required: true }, // Hashed password for security
     last_online: { type: Date },
     account_created_on: { type: Date, default: Date.now },
     location_details: { type: String }, // JSON containing location data when on duty
@@ -15,4 +20,9 @@ const responseAgentSchema = new mongoose.Schema({
     updated_at: { type: Date, default: Date.now },
 });
 
-export const ResponseAgent = mongoose.model('ResponseAgent', responseAgentSchema);
+const ResponseAgentProfile = mongoose.model('ResponseAgentProfile', responseAgentProfileSchema);
+
+const ResponseAgent = mongoose.model('ResponseAgent', responseAgentSchema);
+
+export { ResponseAgent, ResponseAgentProfile };
+
